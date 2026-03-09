653263b5a713d0d58de88583bc55b2abb7025171
zino56 <51100099+zino56@users.noreply.github.com>
2026-03-09 14:53:44 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,12 +3,16 @@ set -eu
 
 mkdir -p /tmp/zc-home
 export HOME=/tmp/zc-home
+export PATH="$HOME/.cargo/bin:$PATH"
 
 : "${LLM_PROVIDER:?Missing LLM_PROVIDER}"
 : "${LLM_MODEL:?Missing LLM_MODEL}"
 : "${LLM_API_KEY:?Missing LLM_API_KEY}"
 : "${TELEGRAM_BOT_TOKEN:?Missing TELEGRAM_BOT_TOKEN}"
 
+command -v zeroclaw
+zeroclaw --version
+
 zeroclaw onboard --provider "$LLM_PROVIDER" --model "$LLM_MODEL" --api-key "$LLM_API_KEY"
 zeroclaw channel add telegram --token "$TELEGRAM_BOT_TOKEN" || true
-exec zeroclaw channel start
+exec zeroclaw daemon
